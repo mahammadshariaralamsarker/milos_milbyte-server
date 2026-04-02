9fdda863b80b84fda59217133a02cc1456abcdd3
mahammadshariaralamsarker <mahammadshariaralam@gmail.com>
2026-04-03 05:30:13 +0600

    feat: restructure Prisma and Auth modules, add Auth functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module'; 
+import { AppService } from './app.service'; 
+import { PrismaModule } from './config/prisma/prisma.module';
+import { AuthModule } from './main/auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
