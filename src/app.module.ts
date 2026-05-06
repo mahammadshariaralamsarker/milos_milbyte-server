cf3b63c7c4ff93385dcc0dde95ff4b9cafffb9b3
mahammadshariaralamsarker <mahammadshariaralam@gmail.com>
2026-05-07 03:17:47 +0600

    refactor: Enhance AppController and AppModule with Swagger decorators and clean up imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,17 @@ import { PrismaModule } from './config/prisma/prisma.module';
 import { AuthModule } from './main/auth/auth.module';
 import { RedisModule } from './config/redis/redis.module';
 import { DeployFilesModule } from './config/deploy-files/deploy-files.module';
-import { StripeModule } from './config/stripe/stripe.module';
-import { SubscriptionModule } from './main/subscription/subscription.module';
-import { HelpRequestModule } from './main/help-request/help-request.module';
+import { DestinationModule } from './main/destination/destination.module';
+import { SeedModule } from './main/maintenance/seed/seed.module';
 
 @Module({
   imports: [
     PrismaModule,
     RedisModule,
     DeployFilesModule,
-    StripeModule,
     AuthModule,
-    SubscriptionModule,
-    HelpRequestModule,
+    DestinationModule,
+    SeedModule,
   ],
   controllers: [AppController],
   providers: [AppService],
